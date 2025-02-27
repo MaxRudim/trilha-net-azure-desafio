ddde98dcb8c7aa0f3cbfd2f45c6b13690e9f63ac
MaxRudim <max.hermsdorff@gmail.com>
2025-02-27 09:22:37 -0300

    Feature: Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -3,9 +3,13 @@ using TrilhaNetAzureDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("ConexaoPadrao");
+
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+
 // Add services to the container.
 builder.Services.AddDbContext<RHContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseMySql(connectionString, serverVersion));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
